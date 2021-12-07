1b887be017f8113f5680426c4f48c2b767ba1672
Sumit yadav <91660827+Sumit9969@users.noreply.github.com>
2021-12-07 17:57:31 +0530

    Update play.py

--- handlers/play.py
+++ handlers/play.py
@@ -120,7 +120,7 @@ async def play(_, message: Message):
     try:
         user = await USER.get_me()
     except:
-        user.first_name = "EsportRobot"
+        user.first_name = "Dark_XMusic_Bot"
     usar = user
     wew = usar.id
     try:
